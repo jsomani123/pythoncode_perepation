f9f82912141d26b8295058962adfcad78947bf22
jsomani123 <86422737+jsomani123@users.noreply.github.com>
2025-04-19 17:54:21 +0530

    Interview prepare

---
+++ assignmet.py
@@ -0,0 +1,7 @@
+pragraph = r""" Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that
+    we are implemeting all the logics by ourself. The aim here is to build our "4 BHK" house with the 
+    help of 'Python programming'. We have total land is of \100 ft * 100ft /, to colmplete the house 
+    we have total 6 labours with 'different skill set like "\\ building wall or building roof \\".
+            I have to print this paragraph as it is given here."""
+
+print(pragraph)
---
+++ print.py
@@ -0,0 +1,25 @@
+length_of_land = 100
+bricks_cost_per_price = 10.5
+labour_mistri = 'Jagmohan'
+is_home = True
+
+
+# if length_of_land==int("100"):
+#     length_of_land=200
+# else :
+#     length_of_land=300
+# print('length_of_land', length_of_land)
+# print('labour name is', labour_mistri)
+# print('''My home is 4 bhk
+# Lenght of land is",length_of_land''')
+# print("My home is \t \"4 bhk\"manish")
+# print(length_of_land,bricks_cost_per_price,labour_mistri,is_home)
+# print(type(length_of_land),type(bricks_cost_per_price),type(labour_mistri),type(is_home))
+#print('length_of_land', length_of_land,"ft")
+# print(f"cost of bricks per unit {bricks_cost_per_price} bricks_cost_per_price ")
+# print("cost of bricks per unit {} {} {}".format(bricks_cost_per_price,length_of_land,labour_mistri))
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+logging.info(f"cost of bricks per unit {bricks_cost_per_price} bricks_cost_per_price")
+                    
+
